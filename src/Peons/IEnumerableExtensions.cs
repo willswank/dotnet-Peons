0bc685181f67409049bc56dcdb0458fb1a96a8e6
Bryan Drenner <bdrenner@new42.org>
2014-07-09 12:06:42 -0400

    IEnumerableExtensions (`IsEmpty()`,`IsNullOrEmpty()`)

---
+++ src/Peons/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Peons
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || enumerable.IsEmpty();
+        }
+
+        public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return !enumerable.Any();
+        }
+    }
+}
---
+++ src/Tests/Peons/IEumerableExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Peons
+{
+	[TestFixture]
+	class IEnumerableExtensionsTests
+	{
+		[Test]
+		public void IsNullOrEmpty_Null_True()
+		{
+            IEnumerable<object> input = null;
+            var output = input.IsNullOrEmpty();
+            Assert.IsTrue(output);
+		}
+
+        [Test]
+        public void IsNullOrEmpty_Empty_True()
+        {
+            var input = new object[] { };
+            var output = input.IsNullOrEmpty();
+            Assert.IsTrue(output);
+        }
+
+        [Test]
+        public void IsNullOrEmpty_NonEmpty_False()
+        {
+            var input = new object[] { new object() };
+            var output = input.IsNullOrEmpty();
+            Assert.IsFalse(output);
+        }
+
+        [Test]
+        public void IsEmpty_Empty_True()
+        {
+            var input = new object[] { };
+            var output = input.IsEmpty();
+            Assert.IsTrue(output);
+        }
+
+        [Test]
+        public void IsEmpty_NonEmpty_False()
+        {
+            var input = new object[] { new object() };
+            var output = input.IsEmpty();
+            Assert.IsFalse(output);
+        }
+	}
+}
