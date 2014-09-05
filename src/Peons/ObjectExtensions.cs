674ed18d8d5ad1a095d76c9c7cc93d146face093
Bryan Drenner <bdrenner@new42.org>
2014-09-05 11:51:54 -0400

    Impl. Object.Exists() extension method

---
+++ src/Peons/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Peons
+{
+    public static class ObjectExtensions
+    {
+        public static bool Exists(this object input)
+        {
+            return input != null;
+        }
+    }
+}
---
+++ src/Tests/Peons/ObjectExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Peons
+{
+    [TestFixture]
+    class ObjectExtensionsTests
+    {
+        [Test]
+        public void Exists_Null_False()
+        {
+            object input = null;
+            var output = input.Exists();
+            Assert.IsFalse(output);
+        }
+
+        [Test]
+        public void Exists_NonNull_True()
+        {
+            object input = new object();
+            var output = input.Exists();
+            Assert.IsTrue(output);
+        }
+    }
+}
