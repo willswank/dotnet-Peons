a65ca2def71d1364be34023722a37a1dc5d290b3
Bryan Drenner <bdrenner@new42.org>
2014-09-30 13:59:23 -0400

    Centralize version info in SolutionInfo.cs

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.2.*")]
